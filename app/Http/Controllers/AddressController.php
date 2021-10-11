177bc41afdfa74cd3746fbaff079d3e1bdee1b5f
KAMONPAT SUNTHONPONG <kamonpat_s@cmu.ac.th>
2021-10-11 20:19:13 +0700

    address controller del method

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -26,15 +26,14 @@ class AddressController extends Controller
                 "addressLine1" => $validated['addressLine1'],
                 "addressLine2" => $validated['addressLine2'],
                 "addressNo" => $validated['addressNo'],
-                "selected" => 0,
                 "city" => $validated['city'],
                 "state" => $validated['state'],
                 "postalCode" => $validated['postCode'],
                 "country" => $validated['country']
             ]);
-            return response(['success'=> true ,'data' => "Insert successfully"],201);
+            return response(['success'=> true ,'data' => "Insert successfully", 'inset' => $validated],201);
         }catch(Exception $e){
-            return response(['success'=> false,'error' => "operation failed"],422);
+            return response(['success'=> false,'error' => "missing requirement"],422);
         }
     }
     public function count($id){
@@ -45,46 +44,48 @@ class AddressController extends Controller
     public function edit(addressReq $req){
         $validated = $req->validated();
         try {
-            DB::table('customerAddresses')
-                ->where('customerID',$validated['customerID'])
-                ->where('addressNo',$validated['addressNo'])
+            customeraddresses::where('CustomerID',$validated['customerID'])
+                ->where('AddressNo',$validated['addressNo'])
                 ->update([
-                "addressLine1" => $validated['addressLine1'],
-                "addressLine2" => $validated['addressLine2'],
-                "city" => $validated['city'],
-                "state" => $validated['state'],
-                "postalCode" => $validated['postCode'],
-                "country" => $validated['country']
+                "AddressLine1" => $validated['addressLine1'],
+                "AddressLine2" => $validated['addressLine2'],
+                "City" => $validated['city'],
+                "State" => $validated['state'],
+                "PostalCode" => $validated['postCode'],
+                "Country" => $validated['country']
             ]);
-            return response(['success'=> true, 'data' => "Edit successfully"],200);
+            return response(['success'=> true, 'data' => $validated],200);
         } catch (Exception $e) {
-            return response(['success'=> false,'error' => "operation failed"],204);
+            return response(['success'=> false,'error' => $e],204);
         }
     }
 
-    public function getAddressNo($id,$no){
-        $addresses = customeraddresses::find($id)
+    public function getAddress($id,$no){
+        $address = customeraddresses::find($id)
         ->where('addressNo',$no)
         ->get();
-        return response(['address'=>$addresses[0]],200);
+        return response(['address'=>$address[0]],200);
     }
 
     public function del($id,$no){
-        $addresses = customeraddresses::find($id);
+        $addresses = customeraddresses::where('CustomerID',$id);
         $count = $addresses->count();
         if($count > 1){
+            $del = $addresses->where('AddressNo',$no)->get();
             $addresses->where('AddressNo',$no)->delete();
-            // DB::table('customerAddresses')
-            // ->find("CustomerID",$id)
-            // ->where("AddressNo",$no)
-            // ->delete();
-            // $i = 1;
-            // foreach($addresses as $address){
-            //     $address->addressNo = $i;
-            //     $address->save();
-            //     $i++;
-            // }
-            return response(['data'=>"deleted",'address' => $addresses],200);
+
+            $addresses = customeraddresses::where('CustomerID',$id)->get();
+            $i = 1;
+            foreach($addresses as $address){
+                DB::table('customerAddresses')
+                ->where('CustomerID',$id)
+                ->where('AddressNo',$address->AddressNo)
+                ->update([
+                        "addressNo" => $i
+                ]);
+                $i++;
+            }
+            return response(['data'=> $del[0]],200);
         }
         return response(['error'=>"must have at least 1 address!!"],204);
     }
