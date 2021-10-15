edb632035c3a9478b1c689989859592587c73c9c
protonnote <nongnumainaka@gmail.com>
2021-10-16 01:32:49 +0700

    full payment

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -12,13 +12,13 @@ class paymentController extends Controller
     public function insert(paymentReq $req){
         $validate = $req->validated();
         try{
-            $payment = payments::create([
+            $payment = payments::insert([
                 'customerNumber' => $validate['customerNumber'],
                 'checkNumber' => $validate['checkNumber'],
                 'paymentDate' => $validate['paymentDate'],
                 'amount' => $validate['amount']
             ]);
-            return response(["success" => false, "message" => "payment successful"]);
+            return response(["success" => true, "message" => "payment successful"]);
         }catch(Exception $e){
             return response(["success" => false, "message" => $e],422);
         }
