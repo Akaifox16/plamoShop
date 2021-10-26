5619220dfcb201b1ff4fc13bf5455f2d968bd11d
Tatsuki <yookskii@gmail.com>
2021-10-26 22:50:52 +0700

    add edit function and stock request

--- app/Http/Controllers/stockController.php
+++ app/Http/Controllers/stockController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\stock_in;
+use App\Http\Requests\stockReq;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -14,7 +14,7 @@ class stockController extends Controller
         return $stock;
     }
  
-    public function create($req){
+    public function create(stockReq $req){
         $validate = $req->validated();
         $timestamp = date('d-m-Y H:i:s');
         try{
@@ -38,6 +38,25 @@ class stockController extends Controller
     public function count($count){
         $_count = stock_in::find($count)->stockcount->count()+1;
         return response(['no'=>$_count],200);
+    }
+
+    public function edit(stockReq $req,$productCode){
+        $timestamp = date('d-m-Y H:i:s');
+        if($req->accepts('application/json')){
+            $validated = $req->validated();
+            try{
+                DB::table('stock_in')->where('productCode',$productCode)
+                ->update([
+                    'productCode'=> $validated['productCode'],
+                    'qty' => $validated ['qty'],
+                    'update_at'=> $timestamp
+                ]);
+                return response(['success'=>true,'data'=>$validated],200);
+            }catch(Exception $e){
+                return response(['success'=>false,'error'=>$e],204);
+            }
+        }
+        return response(['success'=>false,'error'=>'[requied]content-type: json'],204);
     }
 
         
